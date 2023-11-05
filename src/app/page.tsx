747baa34588e8a90b59922e317041238b720b71e
matheus neves <matheusneves1989@gmail.com>
2023-11-05 14:19:57 -0300

    feat(select): implement select component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Input from '@/components/Input'
 import { SettingsTabs } from '@/components/SettingsTabs'
 import { Mail, UploadCloud, User } from 'lucide-react'
 import * as FileInput from '@/components/Form/FileInput'
+import { Select } from '@/components/Form/Select'
+import { SelectItem } from '@/components/Form/Select/SelectItem'
 
 export default function Home() {
   return (
@@ -108,7 +110,10 @@ export default function Home() {
             >
               Country
             </label>
-            <div />
+            <Select placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
@@ -118,7 +123,13 @@ export default function Home() {
             >
               Timezone
             </label>
-            <div />
+            <Select placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (UTC-08:00)"
+              />
+              <SelectItem value="utc3" text="America São Paulo (UTC-03:00)" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
