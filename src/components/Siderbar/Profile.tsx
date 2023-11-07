bf5c6c9bdce8068d8df8f358e10c481e0acbe7ee
matheus neves <matheusneves1989@gmail.com>
2023-11-07 15:44:35 -0300

    feat(button: ghost variant

--- src/components/Siderbar/Profile.tsx
+++ src/components/Siderbar/Profile.tsx
@@ -1,4 +1,5 @@
 import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
 
 export function Profile() {
   return (
@@ -15,12 +16,9 @@ export function Profile() {
           matheusneves1989@gmail.com
         </span>
       </div>
-      <button
-        type="button"
-        className=" ml-auto rounded-md p-2 hover:bg-zinc-50 "
-      >
+      <Button variant="ghost" type="button">
         <LogOut className="h-5 w-5 text-zinc-500" />
-      </button>
+      </Button>
     </div>
   )
 }
