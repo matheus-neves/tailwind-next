06e741189f480cba2a5e68626a6f6cdbadd37b09
matheus neves <matheusneves1989@gmail.com>
2023-11-13 15:32:55 -0300

    feat: dark theme

--- src/components/Siderbar/Profile.tsx
+++ src/components/Siderbar/Profile.tsx
@@ -9,10 +9,10 @@ export function Profile() {
         className="h-10 w-10 rounded-full"
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Matheus Neves
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           matheusneves1989@gmail.com
         </span>
       </div>
